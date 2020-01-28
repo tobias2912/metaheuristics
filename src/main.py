3f6a3c7de006908a631b462876e52728b7be96dd
Tobias Eilertsen <tseilertsen@gmail.com>
2020-01-28 10:26:33 +0100

    generates basic solution

--- src/main.py
+++ src/main.py
@@ -1,8 +1,33 @@
 from reader import Reader
+#solution
+carRoutes = []
+#not visited nodes
+notVisited = []
+reader = Reader()
+freeCalls=[]
 def main():
     print("starting")
-    reader = Reader()
     reader.readfile("Call_7_Vehicle_3.txt")
+    generateSolution()
+    print(carRoutes)
+
+
+def generateSolution():
+    freeCalls=list(range(1, reader.getNumCalls()+1))
+    for carN, home, start, cap in reader.getVehicles():
+        #car n does all possible calls
+        route=[]
+        for callN, origin, dest, size, cost, lowerPickup, upperPickup, lowerDel, upperDel  in reader.getCalls():
+            if isCompatible(carN, callN) and callN in freeCalls:
+                freeCalls.remove(callN)
+                route.append(origin)
+                route.append(dest)
+        carRoutes.append(route)
+
+
+def isCompatible(car, call):
+    compatible = reader.getCompatibleCalls()
+    return call in compatible[car]
 
 
 
@@ -11,9 +36,6 @@ def main():
 
 
 
-
-
-    
 if __name__=="__main__":
     main()
 
--- src/reader.py
+++ src/reader.py
@@ -5,11 +5,46 @@ class Reader:
         self.num_vehicles=0
         self.vehicles = []
         self.calls=[]
+        self.compatibleCalls={}
         self.travel=[]
-        self.nodecost=[]
+        self.nodecost={}
+
+    def getNumCalls(self):
+        """
+        num calls
+        """
+        return self.numCalls
+    def getCompatibleCalls(self):
+        """
+        map from vehicle num to list of compatible calls
+        """
+        return self.compatibleCalls
 
     def getVehicles(self):
+        """
+        vehicle num, home node, start time, capacity
+        """
         return self.vehicles
+
+    def getCalls(self):
+        """
+        index, origin, destination, size, fail cost, lowerbound time pickup , upperbound time pickup, lowerbound time delivery, upperbuond time delivery
+        """
+        return self.calls
+
+    def getVertex(self):
+        """
+        vehicle, origin , destination , travel time, travel cost
+        """
+        return self.travel
+
+    def getNodes(self):
+        """
+        map from (car num, call num) to:
+        origin node time (in hours), origin node costs (in �), destination node time (in hours), destination node costs (in �)
+        time is -1 if not compatible
+        """
+        return self.nodecost
 
     def readfile(self, name):
         fil = open(name)
@@ -18,31 +53,42 @@ class Reader:
         fil.readline()
         num_vehicles=int(fil.readline())
         fil.readline()
-        #vehicle index, home node, starting time, capacity
+        #vehicles
         while(True):
             line=fil.readline()
             if line[0] == '%':
                 break
             vals = [int(x) for x in line.split(",")]
-            self.vehicles.append(vals)
-        #call index, origin node, destination node, size, cost of not transporting, lowerbound timewindow for pickup, upper_timewindow for pickup, lowerbound timewindow for delivery, upper_timewindow for delivery
+            self.vehicles.append(tuple(vals))
+        #num calls
+        self.numCalls = int(fil.readline())
+        fil.readline()
+        #list of calls that can be transported using that vehicle
         while(True):
             line=fil.readline()
             if line[0] == '%':
                 break
             vals = [int(x) for x in line.split(",")]
-            self.calls.append(vals)
-        #travel times and costs: vehicle, origin node, destination node, travel time (in hours), travel cost (in �)
+            self.compatibleCalls[vals[0]]=vals[1:]
+        #calls
         while(True):
             line=fil.readline()
             if line[0] == '%':
                 break
             vals = [int(x) for x in line.split(",")]
-            self.travel.append(vals)
-        #node times and costs: vehicle, call, origin node time (in hours), origin node costs (in �), destination node time (in hours), destination node costs (in �)
+            self.calls.append(tuple(vals))
+        #travel time and cost
         while(True):
             line=fil.readline()
             if line[0] == '%':
                 break
             vals = [int(x) for x in line.split(",")]
-            self.nodecost.append(vals)
+            self.travel.append(tuple(vals))
+        #nodes
+        while(True):
+            line=fil.readline()
+            if line[0] == '%':
+                break
+            vehicle, call, origintime, origincosts, destinationtime, destinationcosts = tuple([int(x) for x in line.split(",")])
+            #self.nodecost.append(tuple(vals))
+            self.nodecost[(vehicle, call)]= (origintime, origincosts, destinationtime, destinationcosts)
